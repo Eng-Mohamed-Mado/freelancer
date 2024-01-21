f956bb62b8c450eaf02eca9efacec840d59735af
Eng-Mohamed-Mado <programmingengineerfuture@gmail.com>
2024-01-21 02:15:34 +0200

    finsh Login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,9 +3,43 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Helpers\ApiResponse;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
 
 class LoginController extends Controller
 {
-    //
+
+    public function login(LoginRequest $request)
+    {
+        try {
+            // Make Manual Validations
+            $validator = $request->validated();
+        } catch (ValidationException $e) {
+            return ApiResponse::sendResponse(422, 'Login Validations Errors', $e->validator->messages()->all());
+        }
+
+        // Check Data Login In For Database
+        if(Auth::attempt(['email'=>$request->email,'password'=>$request->password]))
+        {
+        // After Check And Approve Login
+        $user = Auth::user();
+
+        $data['token'] = $user->createToken('ApiCourse')->plainTextToken;
+        $data['name'] = $user->name;
+        $data['email'] = $user->email;
+        return ApiResponse::sendResponse(200,'User Logged in Successfully', $data);
+
+         }else{
+
+        return ApiResponse::sendResponse(401,' User Credentiats doesn\'t exist ', null);
+        }
+
+
+    }
+ 
 }
--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -3,6 +3,9 @@
 namespace App\Http\Requests\Auth;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Validation\ValidationException;
+use App\Helpers\ApiResponse;
 
 class LoginRequest extends FormRequest
 {
@@ -12,6 +15,23 @@ class LoginRequest extends FormRequest
     public function authorize(): bool
     {
         return true;
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        // Run This Function Is Status Error Validations 422
+        // Any Route EndPoint Afte Api/*
+        if($this->is('api/*'))
+        {
+            // Return Error With Message Only
+            // $response = ApiResponse::sendResponse(422,'Validations Errors',$validator->messages()->all());
+
+            // Return Error With Message And Name Faild
+            $response = ApiResponse::sendResponse(422,'Login Validations Errors',$validator->errors());
+
+            // Pass Error With Style Data In Method Validations Exceptions
+            throw new ValidationException($validator,$response);
+        }
     }
 
     /**
